619f8e628316c498910c8667e75289d9b162e955
shardavishwakarma <shardavishwakarma20@gmail.com>
2022-12-01 07:35:27 +0530

    first commit

---
+++ src/main/java/in/rani/AngularWelcomeMsgIntegrationApplication.java
@@ -0,0 +1,13 @@
+package in.rani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularWelcomeMsgIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularWelcomeMsgIntegrationApplication.class, args);
+	}
+
+}
